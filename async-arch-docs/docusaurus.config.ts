6b306d182e19b030a19a14dfb76755d973d7ba18
Denis Tretyakov <d.a.tretyakov@tinkoff.ru>
2024-02-14 11:08:26 +0500

    fix docs configs

--- async-arch-docs/docusaurus.config.ts
+++ async-arch-docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://denisqq.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/async-arch-courses',
+  baseUrl: '/async-arch-courses/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
